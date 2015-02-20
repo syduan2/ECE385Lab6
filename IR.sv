dda08ed45e72a07eacfcf8ccba8edfac755a8e16
jiajames <James-OSX@Jamess-MacBook-Air.local>
2015-02-20 16:52:14 -0600

    did a little thingys

--- IR.sv
+++ IR.sv
@@ -1,8 +1,13 @@
 module IR (input Clk, Reset,
 				  LD_IR,
 			input [15:0] D_in,
-			output [15:0] D_out);
+			output [15:0] D_out,
+			output logic ContinueIR);
 
-reg_16 MAR_reg (.Clk, .Load(LD_IR), .Reset, .D_In, .R(D_Out));
+reg_16 IR_reg (.Clk, .Load(LD_IR), .Reset, .D_In, .R(D_Out));
+
+	always_comb
+	begin
+
 
 endmodule
--- processor.sv
+++ processor.sv
@@ -8,4 +8,38 @@ module processor (input Clk, Reset, Run, Continue,
 				  output logic [19:0] ADDR,
 				  inout logic [15:0] Data)
 
+	logic 	LD_MAR,
+			LD_MDR,
+			LD_IR,
+			LD_BEN,
+			LD_CC,
+			LD_REG,
+			LD_PC,
+
+	logic GatePC,
+		  GateMDR,
+		  GateALU,
+		  GateMARMUX,
+
+	logic [1:0] PCMUX,
+                DRMUX,
+				SR1MUX,
+	logic SR2MUX,
+		  ADDR1MUX,
 	
+	logic [1:0] ADDR2MUX,
+
+	logic MARMUX,
+
+	logic Mem_CE,
+		  Mem_UB,
+	   	  Mem_LB,
+		  Mem_OE,
+	   	  Mem_WE,
+
+	ISDU control
+	IR instruct_reg
+	MAR address_reg
+	MDR data_reg
+	PC prog_counter
+	HexDriver hex_disp
