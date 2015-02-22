9b98426c0b5050e5c76ba61272a2f83ca52be299
sunnyddelight <sunnyddelight@gmail.com>
2015-02-21 18:23:47 -0600

    swaglife

--- processor.sv
+++ processor.sv
@@ -1,12 +1,16 @@
 module processor (input Clk, Reset, Run, Continue,
 				  input [15:0] S,
-				  output logic [11:0] LED,
 				  output logic [6:0] HEX0,
 				  					 HEX1,
 				  					 HEX2,
 				  					 HEX3,
-				  output logic [19:0] ADDR,
-				  inout logic [15:0] Data);
+				  output logic [15:0] ADDR,
+				  inout logic [15:0] Data,
+				  output logic Mem_CE_out,
+									Mem_UB_out,
+									Mem_LB_out,
+									Mem_OE_out,
+									Mem_WE_out);
 
 	wire 	LD_MAR,
 			LD_MDR,
@@ -50,8 +54,10 @@ module processor (input Clk, Reset, Run, Continue,
 	
 	//PC
 	reg_16 PC(.*, .Load(LD_PC), .Reset(0), .D_in(PC_in), .R(PC_out));
-	mux_4x1(.In0(PC_1), .In1(16'b0), .In2(Bus), .In3(16'b0), .select(PCMUX), .Out(PC_in));
+	mux_4x1(.In0(PC_out_1), .In1(16'b0), .In2(Bus), .In3(16'b0), .select(PCMUX), .Out(PC_in));
 	tri_buffer_16 pc_gate(.Enable(GatePC), .In(PC), .Out(Bus));
+	carry_select_adder_16(.A(PC_out), .B(16'b0), .c_in(1'b1), .c_out(1'bz), .S(PC_out_1));
+	
 	
 	//MDR
 	reg_16 MDR(.*, .Load(LD_MDR), .Reset(0), .D_in(MDR_in), .R(MDR_out));
@@ -60,6 +66,17 @@ module processor (input Clk, Reset, Run, Continue,
 	tri_buffer_16 mdr_gate_SRAM(.Enable(GateMDR), .In(MDR_out), .Out(SRAM_bus));
 	
 	reg_16 MAR(.*, .Load(LD_MAR), .Reset(0), .D_in(Bus), .R(MAR_out));
-
+	
+	HexDriver out0(.In0(IR_out[3:0]), .Out0(HEX0));
+	HexDriver out1(.In0(IR_out[7:4]), .Out0(HEX1));
+	HexDriver out2(.In0(IR_out[11:8]), .Out0(HEX2));
+	HexDriver out3(.In0(IR_out[15:12]), .Out0(HEX3));
+	assign ADDR=MAR_out;
+	assign Data=MDR_out;
+	assign Mem_CE_out=Mem_CE;
+	assign Mem_UB_out=Mem_UB;
+	assign Mem_LB_out=Mem_LB;
+	assign Mem_OE_out=Mem_OE;
+	assign Mem_WE_out=Mem_WE;
 	
 endmodule
